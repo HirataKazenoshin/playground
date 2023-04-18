1708517336a410856409daa1719eb9a77520301f
kazenoshin <hiratakazenoshin@gmail.com>
2023-04-18 21:32:06 +0900

    javabook2 first commit!

---
+++ java-book2/src/plactice/list3_1.java
@@ -0,0 +1,15 @@
+package plactice;
+
+import java.util.ArrayList;
+
+public class list3_1 {
+	public static void main(String[] args) {
+		ArrayList<Integer> points = new ArrayList<Integer>();
+		points.add(10);
+		points.add(80);
+		points.add(75);
+		for(int i : points) {
+			System.out.println(i);
+		}
+	}
+}
