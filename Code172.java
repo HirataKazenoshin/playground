7ec323a5ca0c0ba949fcd924e11d730db062135b
kazenoshin <hiratakazenoshin@gmail.com>
2023-05-13 23:11:33 +0900

    PATH is difficult for me

---
+++ Code172.java
@@ -0,0 +1,12 @@
+// import java.io.*;
+
+public class Code172 {
+    public static void main(String[] args){
+        // try {
+            // FileWriter fw = new FileWriter("data.txt");
+            System.out.println();
+        // } catch (IOException e){
+        //     System.out.println("エラーが発生しました");
+        // }
+    }
+}
