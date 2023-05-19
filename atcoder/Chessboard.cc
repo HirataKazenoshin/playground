d7c08639c33874b649fd34dcfd4e01b3b05a4f96
kazenoshin <hiratakazenoshin@gmail.com>
2023-05-19 14:01:16 +0900

    Cheseboard

---
+++ atcoder/Chessboard.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  char chess[8][8];
+  for(int beside = 0; beside < 8; beside++){
+    for(int vertical = 0; vertical < 8; vertical++){
+      cin >> chess[]
+    }
+  }
+}
