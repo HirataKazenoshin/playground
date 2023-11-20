859aff83362ad2889c8d89bcccadeae17cb6f399
kazenoshin <hiratakazenoshin@gmail.com>
2023-11-20 22:13:21 +0900

    atcoder

---
+++ atcoder/Power.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Power {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    
+    int ans = 1;
+    for (int i = 0; i < b; i++){
+      ans *= a;
+    }
+    System.out.println(ans);
+  }
+}
