be3e250b41e6c4f106093f09605f983850faf68a
Noshin <hiratakazenoshin@gmail.com>
2025-04-12 12:55:39 +0900

    LinearSerch

---
+++ bookCode/A02/LinearSerch.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, X;
+  cin >> N >> X;
+  int tmp;
+  for (int i = 0; i < N; i++) {
+    cin >> tmp;
+    if (X == tmp) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+  return 0;
+}
